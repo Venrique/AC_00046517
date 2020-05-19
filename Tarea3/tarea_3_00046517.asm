b244cf39eee2df8b9b8eb889a1bc77237eda9b44
venrique <00046517@uca.edu.sv>
2020-05-19 15:12:13 -0600

    texto de prueba mostrado

--- Tarea3/tarea_3_00046517.asm
+++ Tarea3/tarea_3_00046517.asm
@@ -1,19 +1,54 @@
-	org	100
+	org	100h
 
 section	.text
 	call	texto
-	call	cursor
 	call	frase
 	call	espera
 
-texto:
+	int	20h
 
-cursor:
+texto:	mov	ah, 00h
+	mov	al, 03h
+	int	10h
+	ret
 
-frase:
+m_cur:	mov	ah, 02h
+	mov	dx, di
+	add	dl, 10d
+	mov	dh, 5d
+	mov	bh, 0h
+	int	10h
+	ret
 
-espera:
+write:	mov	ah, 09h
+	mov	al, cl
+	mov	bh, 0h
+	mov	bl, 00001111b
+	mov	cx, 1h
+	int	10h
+	ret
+
+frase:	mov	di, 0d
+	
+lopi:	mov	cl, [uno+di]
+	call	m_cur
+	call	write
+	inc	di
+	cmp	di, len
+	jb	lopi
+	ret
+
+espera:	mov	ax, 0000h
+	int	16h
+	ret
 
 section	.data
 
-comnt	db	"Texto de prueba"
+uno	db	"El ayer es historia..."
+dos	db	"... El mañana es un misterio"
+tres	db	"Pero el hoy es un obsequio"
+cuatro	db	"Por eso se llama presente."
+len	equ	$-uno
+lend	equ	$-dos
+lent	equ	$-tres
+lenc	equ	$-cuatro
